9423dfd6076a6380e2933b0e3594b3970a893d04
Vo7kov <jegor.volkov.a@gmail.com>
2024-05-01 23:57:47 +0200

    feat(MASTER): brands

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,17 @@ import { Features } from '~/pages/HomePage';
 import { Container } from '~/ui/Container';
 
 import banner from '../../public/assets/banner.jpg';
+import brands from '../../public/assets/brands.jpg';
 
 const Home: React.FC = React.memo(() => {
   return (
     <div className="flex flex-col gap-8">
       <Container className="mt-6">
         <Image src={banner} alt="banner" />
+      </Container>
+
+      <Container className="mt-6">
+        <Image src={brands} alt="brands" />
       </Container>
 
       <Features />
