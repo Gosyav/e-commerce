88a6bcf1d587a0a9497bcc6d14950947c56c13f3
Vo7kov <jegor.volkov.a@gmail.com>
2024-05-01 23:04:02 +0200

    fix(MASTER): split burger into separate component

---
+++ src/modules/Header/components/Burger/Burger.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { Container } from '~/ui/Container';
+
+import arrowIcon from '../../../../../public/assets/arrow.svg';
+import closeIcon from '../../../../../public/assets/close.svg';
+import logo from '../../../../../public/assets/logo.svg';
+
+type Props = {
+  setIsBurgerOpened: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+// eslint-disable-next-line react/display-name
+export const Burger: React.FC<Props> = React.memo(({ setIsBurgerOpened }) => {
+  const navLinks = [
+    {
+      name: 'test',
+      path: '/wher1e',
+    },
+    {
+      name: 'test11',
+      path: '/where',
+    },
+  ];
+
+  return (
+    <Container className="fixed bottom-0 left-0 right-0 top-0 bg-white py-4">
+      <div className="flex items-center justify-between border-b-2 pb-6">
+        <Image src={logo as string} alt="logo" />
+
+        <button
+          onClick={() => setIsBurgerOpened(false)}
+          type="button"
+          className="h-6 w-6"
+        >
+          <Image src={closeIcon as string} alt="close" />
+        </button>
+      </div>
+
+      <div className="mt-6 flex flex-col gap-3">
+        {navLinks.map((link) => (
+          <div
+            className="flex items-center justify-between text-base md:text-xl"
+            key={link.path}
+          >
+            <Link href={link.path} className="pr-3.5">
+              {link.name}
+            </Link>
+
+            <Image src={arrowIcon as string} alt="arrow" className="h-6 w-6" />
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
+});
--- src/modules/Header/components/Header/Header.tsx
+++ src/modules/Header/components/Header/Header.tsx
@@ -4,29 +4,16 @@ import React, { useState } from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
+import { Burger } from '~/modules/Header/components/Burger/Burger';
 import { Container } from '~/ui/Container';
 
-import arrowIcon from '../../../../../public/assets/arrow.svg';
 import burger from '../../../../../public/assets/burger.svg';
-import closeIcon from '../../../../../public/assets/close.svg';
-import logo from '../../../../../public/assets/logo.svg';
 import shoppingCart from '../../../../../public/assets/shoppingCart.svg';
 import userIcon from '../../../../../public/assets/user.svg';
 
 // eslint-disable-next-line react/display-name
 export const Header: React.FC = React.memo(() => {
   const [isBurgerOpened, setIsBurgerOpened] = useState(false);
-
-  const navLinks = [
-    {
-      name: 'test',
-      path: '/where',
-    },
-    {
-      name: 'test',
-      path: '/where',
-    },
-  ];
 
   return (
     <header>
@@ -77,40 +64,7 @@ export const Header: React.FC = React.memo(() => {
         </div>
       </Container>
 
-      {isBurgerOpened && (
-        <Container className="fixed bottom-0 left-0 right-0 top-0 bg-white py-4">
-          <div className="flex items-center justify-between border-b-2 pb-6">
-            <Image src={logo as string} alt="logo" />
-
-            <button
-              onClick={() => setIsBurgerOpened(false)}
-              type="button"
-              className="h-6 w-6"
-            >
-              <Image src={closeIcon as string} alt="close" />
-            </button>
-          </div>
-
-          <div className="mt-6 flex flex-col gap-3">
-            {navLinks.map((link) => (
-              <div
-                className="flex items-center justify-between text-base md:text-xl"
-                key={link.path}
-              >
-                <Link href={link.path} className="pr-3.5">
-                  {link.name}
-                </Link>
-
-                <Image
-                  src={arrowIcon as string}
-                  alt="arrow"
-                  className="h-6 w-6"
-                />
-              </div>
-            ))}
-          </div>
-        </Container>
-      )}
+      {isBurgerOpened && <Burger setIsBurgerOpened={setIsBurgerOpened} />}
     </header>
   );
 });
