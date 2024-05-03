403d5906d0badd7ebd5b3802973383f7d2153f23
Vo7kov <jegor.volkov.a@gmail.com>
2024-05-03 23:11:32 +0200

    fix(MASTER): fix not found product page

--- src/app/(pages)/[category]/[id]/page.tsx
+++ src/app/(pages)/[category]/[id]/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 
 import Image from 'next/image';
-import { useParams } from 'next/navigation';
+import { notFound, useParams } from 'next/navigation';
 import { Characteristics } from '~/pages/ProductPage';
 import { api } from '~/trpc/react';
 import { Container } from '~/ui/Container';
@@ -15,20 +15,20 @@ const ProductPage: React.FC = React.memo(() => {
 
   const product = api.product.getById.useQuery({ id }).data;
 
+  if (!product) {
+    return notFound();
+  }
+
   return (
     <div className="h-full">
       <Container className="mt-4 grid h-full grid-rows-[min-content_1fr_min-content] items-center gap-5 md:grid-cols-2 md:grid-rows-[min-content_min-content]">
         <div className="order-2 md:order-none md:col-start-1 md:col-end-2">
           <h1 className="text-3xl font-medium">{product?.title}</h1>
 
-          <p className="mt-4 block text-lg font-light">
-            {product?.description}
-          </p>
+          <p className="mt-4 block text-lg font-light">{product.description}</p>
 
           <div className="mt-4 flex items-center justify-between">
-            <p className="text-3xl font-bold">
-              {(product?.price as number) * count} грн
-            </p>
+            <p className="text-3xl font-bold">{product.price * count} грн</p>
 
             <div className="flex items-center gap-2 rounded bg-color-three p-4 text-white">
               <button
@@ -71,13 +71,13 @@ const ProductPage: React.FC = React.memo(() => {
         </div>
 
         <div className="order-3 mt-6 md:order-none md:col-start-1 md:col-end-3">
-          <Characteristics product={product!} />
+          <Characteristics product={product} />
         </div>
 
         <div className="order-1 flex justify-end md:order-none md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-1">
           <Image
-            src={product?.imgUrl as string}
-            alt={product?.title as string}
+            src={product.imgUrl}
+            alt={product.title}
             width={500}
             height={500}
           />
